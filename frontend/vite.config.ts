2c67af67f3a941b8723de43a84cda959a697deac
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2023-05-19 00:48:50 -0300

    fix(vite): add correct repo name

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio',
+  base: '/github-portoflio-win95',
   plugins: [react()],
   resolve: {
     alias: {
